7d9d1ed82276de746e03364f9bfe108e14dab150
Ramy Atalla <ramywadia@gmail.com>
2020-06-28 15:33:15 +0200

    .gitignore

--- Covid19Tracker/Model/Country.swift
+++ Covid19Tracker/Model/Country.swift
@@ -9,10 +9,10 @@
 import Foundation
 
 struct Country: Codable, Hashable {
-var country: String
-let cases: Cases
-let deaths: Deaths
-let tests: Tests
+    var country: String
+    let cases: Cases
+    let deaths: Deaths
+    let tests: Tests
 }
 
 
